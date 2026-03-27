2bfb3bcff5f46df74ad37d6458197923916e025c
Zakii <zakii@360-search.com>
2026-03-27 11:42:32 -0400

    Create state.js

---
+++ core/state.js
@@ -0,0 +1,22 @@
+const G = {
+  cash: 500,
+  bank: 0,
+  rating: 50,
+  heat: 0,
+  energy: 100,
+
+  day: 1,
+  hour: 8,
+
+  job: null,
+  housing: null,
+
+  inventory: [],
+  wardrobe: [],
+  cars: [],
+
+  portfolio: [],
+  businesses: [],
+
+  location: "Midtown"
+};
